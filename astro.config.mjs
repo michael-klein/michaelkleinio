cbf343540afe7daa936dd47167dc1ebcd6f92e67
Michael Klein <32208506+michael-klein@users.noreply.github.com>
2024-12-01 12:08:06 +0100

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: cloudflare()
+});
